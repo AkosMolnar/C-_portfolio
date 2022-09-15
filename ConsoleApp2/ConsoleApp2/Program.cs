f79c26763c38a7619270e3a708f14dcce27408df
AkosMolnar <99802807+AkosMolnar@users.noreply.github.com>
2022-09-15 09:22:49 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        class Test
+        {
+            static int a = 0;
+            public Test()
+            {
+                Console.WriteLine(++a);
+            }
+            ~Test()
+            {
+                a--;
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Test t = new Test();
+            }
+        }
+    }
+}
